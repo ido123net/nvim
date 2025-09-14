52a1865fa8d72d2770afc0e58198204d24f2991e
Ido Frenkel <ido123net@gmail.com>
2025-09-14 10:39:38 +0300

    switch tmux with shift

--- lua/plugins/tmux-navigator.lua
+++ lua/plugins/tmux-navigator.lua
@@ -1,3 +1,18 @@
 return {
   "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-s-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-s-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-s-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-s-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-s-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
 }
