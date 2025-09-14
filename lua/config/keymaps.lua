a584b0930c700dba5b67b41124d128cf8930b79b
Ido Frenkel <ido123net@gmail.com>
2025-09-14 09:08:34 +0300

    Add tmux navigator plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,9 +5,3 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Escape with ctrl-C in insert mode.
 vim.keymap.set('i', '<C-c>', '<ESC>')
 
--- keymap for navigating split screens
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
